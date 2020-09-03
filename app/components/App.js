85bb948890c6d925ad80070650acbd0bee8076c1
vaishram <ramakant.vaishnav@dspim.com>
2020-09-03 21:15:30 +0530

    added toggle and refresh logic

--- app/components/App.js
+++ app/components/App.js
@@ -16,92 +16,84 @@ export default class App extends Component {
       response : [],
       dataApi: [],
       error: false,
-      isLoading : true
+      isLoading : true,
+      buttonProp : false
     }
   }
+
   componentWillMount() {
-    Axios.get('https://api.spacexdata.com/v3/launches?limit=100')
-      .then((result) => {
-        this.setState({
-          dataApi: result.data,
-          response: result.data,
-          error: false,
-          isLoading: false
-        })
-      })
-      .catch((error) => {
-        console.log("eror", error)
-        this.setState({
-          dataApi: [],
-          error: true,
-          isLoading: false
-        })
-      })
+    
+    let url = this.getUrlBasedOnfilter()
+    this.fetchMissionData(url)
+    
   }
 
 
   handleYearClick = (year) => {
 
-    let selectedLaunch = this.state.selectedLaunch
-    this.setState({
-      selectedYear : year,
-      isLoading: true
-    },()=>{
+    let selectedYear = this.state.selectedYear
 
-      if(selectedLaunch){
-        this.getAllData()
-      }
-      else{
-        Axios.get(`https://api.spaceXdata.com/v3/launches?limit=100&launch_year=${year}`)
-        .then((result) => {
-          console.log("=====result",result)
-          this.setState({
-            dataApi: result.data,
-            response: result.data,
-            error: false,
-            isLoading: false
-          })
-        })
-        .catch((error) => {
-          this.setState({
-            dataApi: [],
-            error: true,
-            isLoading: false
-          })
-        })
-      }
-    })
+    if(selectedYear === year){
+      this.setState({
+        selectedYear : null
+      },()=>{
+        sessionStorage.removeItem('selectedYear');
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
+    else{
+      this.setState({
+        selectedYear : year
+      },()=>{
+        sessionStorage.setItem('selectedYear',year);
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
+
   }
 
   handleLaunchClick = (launch) => {
-    let selectedYear = this.state.selectedYear
-    this.setState({
-      selectedLaunch : launch,
-      isLoading: true
-    },()=>{
 
-      if(selectedYear){
-        this.getAllData()
-      }
-      else{
-        Axios.get(`https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${launch}`)
-        .then((result) => {
-          this.setState({
-            dataApi: result.data,
-            response: result.data,
-            error: false,
-            isLoading: false
-          })
-        })
-        .catch((error) => {
-          this.setState({
-            dataApi: [],
-            error: true,
-            isLoading: false
-          })
-        })
-      }
-    })
+
+    let selectedLaunch = this.state.selectedLaunch
+
+    if(selectedLaunch === launch){
+      this.setState({
+        selectedLaunch : null
+      },()=>{
+        sessionStorage.removeItem('selectedLaunch');
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
+    else{
+      this.setState({
+        selectedLaunch : launch
+      },()=>{
+        sessionStorage.setItem('selectedLaunch',launch);
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
+  }
+
+  handleLandClick = (land) => {
+    let selectedLand = this.state.selectedLand
+
+    if(selectedLand === land){
+      this.setState({
+        selectedLand : null
+      },()=>{
+        sessionStorage.removeItem('selectedLand');
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
+    else{
+      this.setState({
+        selectedLand : land
+      },()=>{
+        sessionStorage.setItem('selectedLand',land);
+        this.fetchMissionData(this.getUrlBasedOnfilter())
+      })
+    }
   }
 
   getAllData = () => {
@@ -129,6 +121,82 @@ export default class App extends Component {
     
   }
 
+  fetchMissionData = (url) => {
+
+    this.setState({
+      isLoading:true
+    },()=>{
+      Axios.get(url)
+      .then((result) => {
+        this.setState({
+          dataApi: result.data,
+          response: result.data,
+          error: false,
+          isLoading: false,
+          buttonProp : !this.state.buttonProp
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          dataApi: [],
+          error: true,
+          isLoading: false,
+          buttonProp : !this.state.buttonProp
+        })
+      })
+    })
+   
+  }
+
+  getUrlBasedOnfilter=()=>{
+    let year = sessionStorage.getItem('selectedYear')
+    let launch = sessionStorage.getItem('selectedLaunch')
+    let land = sessionStorage.getItem('selectedLand')
+
+    let url = ''
+    if(!year && !launch &&  !land){
+        //fetch all with no filter
+        url = `https://api.spacexdata.com/v3/launches?limit=100`
+       
+    }
+    else if(year && launch &&  land){
+          //fetch all  filter
+          url = `https://api.spacexdata.com/v3/launches?
+          limit=100&launch_success=${launch}&land_success=${land}&launch_year=${year}`
+         
+    }
+    else if(year && launch &&  !land){
+      //fetch year and launch
+      url = `https://api.spacexdata.com/v3/launches?
+      limit=100&launch_success=${launch}&launch_year=${year}`
+          
+    }
+    else if(year && land && !launch )  {
+      //fetch year and launch
+      url = `https://api.spacexdata.com/v3/launches?
+      limit=100&land_success=${land}&launch_year=${year}`
+    }
+    else if( launch &&  land && !year ){
+      url = `https://api.spacexdata.com/v3/launches?
+      limit=100&launch_success=${launch}&land_success=${land}`
+    }
+    else if(year && !launch &&  !land){
+      url = `https://api.spacexdata.com/v3/launches?
+      limit=100&launch_year=${year}`
+    }
+    else if(launch && !year && !land  )  {
+      url = `https://api.spacexdata.com/v3/launches?
+      limit=100&launch_success=${launch}`
+    }
+    else if( land && !launch &&   !year ){
+      url = `https://api.spacexdata.com/v3/launches?
+        limit=100&land_success=${land}`
+    }
+   
+    return url;
+    
+  }
+
   render() {
     let { dataApi,isLoading } = this.state;
     return (
@@ -139,9 +207,10 @@ export default class App extends Component {
         <div className={styles.container}>
           <div className={styles.menu}>
             <MenuComponent 
+              buttonProp={this.state.buttonProp}
               handleYearClick={this.handleYearClick} 
               handleLaunchClick={this.handleLaunchClick}
-              handleLandingClick={this.handleLandingClick}
+              handleLandClick={this.handleLandClick}
               />
           </div>
           <div className={styles.content}>
--- app/components/menu/MenuComponent.js
+++ app/components/menu/MenuComponent.js
@@ -2,7 +2,7 @@
 import React,{Component} from 'react'
 import Styles from './styles.css'
 
-const buttonStyle={margin:5, backgroundColor:'#d9fad9', borderColor: 'honeydew'}
+
 
 export default class MenuComponent extends Component{
 
@@ -14,9 +14,26 @@ export default class MenuComponent extends Component{
         }
       }
 
-      
+        getYearbuttonStyle=(value)=>   {
+
+                let year = sessionStorage.getItem('selectedYear')
+                return {margin:5, backgroundColor: (value === year ? '#639453' : '#d9fad9'), borderColor: 'honeydew'}
+        } 
+        getLaunchbuttonStyle=(value)=>   {
+
+           
+            let launch = sessionStorage.getItem('selectedLaunch')
+            return {margin:5, backgroundColor:(value === launch ? '#639453' : '#d9fad9' ), borderColor: 'honeydew'}
+        } 
+        getLandbuttonStyle=(value)=>   {
+
+            let land = sessionStorage.getItem('selectedLand')
+            return {margin:5, backgroundColor:(value === land ? '#639453' : '#d9fad9' ), borderColor: 'honeydew'}
+        } 
 
      render(){
+        
+            console.log("====here");
          return(
              <div className={Styles.container}>
                  <div>Filters</div>
@@ -31,35 +48,35 @@ export default class MenuComponent extends Component{
                         }}
                     />
                      <div>
-                        <button onClick={()=>this.props.handleYearClick('2006')} style={buttonStyle}>2006</button>
-                        <button onClick={()=>this.props.handleYearClick('2007')} style={buttonStyle}>2007</button>
+                        <button onClick={()=>this.props.handleYearClick('2006')} style={this.getYearbuttonStyle(2006)}>2006</button>
+                        <button onClick={()=>this.props.handleYearClick('2007')} style={this.getYearbuttonStyle(2007)}>2007</button>
                      </div>
                      <div>
-                         <button onClick={()=>this.props.handleYearClick('2008')} style={buttonStyle}>2008</button>
-                         <button onClick={()=>this.props.handleYearClick('2009')} style={buttonStyle}>2009</button>
+                         <button onClick={()=>this.props.handleYearClick('2008')} style={this.getYearbuttonStyle(2008)}>2008</button>
+                         <button onClick={()=>this.props.handleYearClick('2009')} style={this.getYearbuttonStyle(2009)}>2009</button>
                      </div>
                      <div>
-                         <button onClick={()=>this.props.handleYearClick('2010')} style={buttonStyle}>2010</button>
-                         <button onClick={()=>this.props.handleYearClick('2011')} style={buttonStyle}>2011</button>
+                         <button onClick={()=>this.props.handleYearClick('2010')} style={this.getYearbuttonStyle(2010)}>2010</button>
+                         <button onClick={()=>this.props.handleYearClick('2011')} style={this.getYearbuttonStyle(2011)}>2011</button>
                     </div>
                      <div>
-                         <button onClick={()=>this.props.handleYearClick('2012')} style={buttonStyle}>2012</button>
-                         <button onClick={()=>this.props.handleYearClick('2013')} style={buttonStyle}>2013</button>
+                         <button onClick={()=>this.props.handleYearClick('2012')} style={this.getYearbuttonStyle(2012)}>2012</button>
+                         <button onClick={()=>this.props.handleYearClick('2013')} style={this.getYearbuttonStyle(2013)}>2013</button>
                     </div>
                     <div>
-                         <button onClick={()=>this.props.handleYearClick('2014')} style={buttonStyle}>2014</button>
-                         <button onClick={()=>this.props.handleYearClick('2015')} style={buttonStyle}>2015</button>
+                         <button onClick={()=>this.props.handleYearClick('2014')} style={this.getYearbuttonStyle(2014)}>2014</button>
+                         <button onClick={()=>this.props.handleYearClick('2015')} style={this.getYearbuttonStyle(2015)}>2015</button>
                     </div>
                      <div>
-                         <button onClick={()=>this.props.handleYearClick('2016')} style={buttonStyle}>2016</button>
-                         <button onClick={()=>this.props.handleYearClick('2017')} style={buttonStyle}>2017</button>
+                         <button onClick={()=>this.props.handleYearClick('2016')} style={this.getYearbuttonStyle(2016)}>2016</button>
+                         <button onClick={()=>this.props.handleYearClick('2017')} style={this.getYearbuttonStyle(2017)}>2017</button>
                     </div>
                      <div>
-                        <button onClick={()=>this.props.handleYearClick('2018')} style={buttonStyle}>2018</button>
-                        <button onClick={()=>this.props.handleYearClick('2019')} style={buttonStyle}>2019</button>
+                        <button onClick={()=>this.props.handleYearClick('2018')} style={this.getYearbuttonStyle(2018)}>2018</button>
+                        <button onClick={()=>this.props.handleYearClick('2019')} style={this.getYearbuttonStyle(2019)}>2019</button>
                     </div>
                      <div>
-                         <button onClick={()=>this.props.handleYearClick('2020')} style={buttonStyle}>2020</button>
+                         <button onClick={()=>this.props.handleYearClick('2020')} style={this.getYearbuttonStyle(2020)}>2020</button>
                     </div>
                      
                  </div>
@@ -76,10 +93,29 @@ export default class MenuComponent extends Component{
                     /></div>
                      
                      <div>
-                         <button onClick={()=>this.props.handleLaunchClick(true)}style={buttonStyle}>True</button>
-                        <button onClick={()=>this.props.handleLaunchClick(false)} style={buttonStyle}>false</button>
+                         <button onClick={()=>this.props.handleLaunchClick(true)}style={this.getLaunchbuttonStyle(true)}>True</button>
+                        <button onClick={()=>this.props.handleLaunchClick(false)} style={this.getLaunchbuttonStyle(false)}>false</button>
                     </div>
                  </div>
+                 <div className={Styles.launchContainer} >
+                    <div>
+                    <p>Successful Landing </p>
+                        <hr
+                        style={{
+                            color: 'black',
+                            backgroundColor: 'black',
+                            height: 1,
+                            width : '100%'
+                        }}
+                    /></div>
+                     
+                     <div>
+                         <button onClick={()=>this.props.handleLandClick(true)}style={this.getLandbuttonStyle(true)}>True</button>
+                        <button onClick={()=>this.props.handleLandClick(false)} style={this.getLandbuttonStyle(false)}>false</button>
+                    </div>
+                 </div>
+
+                 
             </div>
          )
      } 
